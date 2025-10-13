4b575b17227247c89aac128fd91863d2b8a90d32
Anna <anna@vanharmelen.me>
2025-10-13 11:46:39 +0200

    expand main analysis for poster but also add median split

--- GA_saccadeBias.m
+++ GA_saccadeBias.m
@@ -5,16 +5,18 @@
 clear; clc; close all;
     
 %% parameters
-pp2do           = [1:8];
+pp2do           = [1:9,11:26];
 oneOrTwoD       = 1;        oneOrTwoD_options = {'_1D','_2D'};
 nsmooth         = 200;
-plotSinglePps   = 1;
-plotGAs         = 1;
-plotFigures     = 1;
+plotSinglePps   = 0;
+plotGAs         = 0;
+plotFigures     = 0;
 xlimtoplot      = [-100 1500];
 
 %% set visual parameters
 [bar_size, colours, dark_colours, labels, subplot_size, percentageok] = setBehaviourParam(pp2do);
+
+avg_effect_labels = {'fast congruent', 'slow congruent', 'fast incongruent', 'slow incongruent'};
 
 %% load and aggregate the data from all pp
 s = 0;
@@ -27,6 +29,12 @@ for pp = pp2do
     % load
     disp(['getting data from participant ', param.subjName]);
     load([param.path, '\saved_data\saccadeEffects', oneOrTwoD_options{oneOrTwoD} '__', param.subjName], 'saccade','saccadesize');
+
+    % save averages (saccade effect (capture cue effect and probe cue reaction)
+    avg_saccade_effect(s, 1) = mean(saccade.effect(12,saccade.time>=200 & saccade.time<=600));
+    avg_saccade_effect(s, 2) = mean(saccade.effect(16,saccade.time>=200 & saccade.time<=600));
+    avg_saccade_effect(s, 3) = mean(saccade.effect(13,saccade.time>=200 & saccade.time<=600));
+    avg_saccade_effect(s, 4) = mean(saccade.effect(17,saccade.time>=200 & saccade.time<=600));
        
     % smooth?
     if nsmooth > 0
@@ -62,7 +70,18 @@ saccadesize.effect = squeeze(mean(d6));
 
 %% all subs
 if plotSinglePps
-    % toward vs away
+    % effect
+    figure;
+    for sp = 1:s
+        subplot(subplot_size,subplot_size,sp); hold on;
+        plot(saccade.time, squeeze(d3(sp,5,:)));
+        plot(xlim, [0,0], '--k');
+        xlim(xlimtoplot); ylim([-1.5 1.5]);
+        title(pp2do(sp));
+    end
+    legend(saccade.label);
+
+    % toward vs. away
     figure;
     for sp = 1:s
         subplot(subplot_size,subplot_size,sp); hold on;
@@ -139,6 +158,45 @@ if plotGAs
     xlim(xlimtoplot);
     ylabel('Rate (Hz)');
     xlabel('Time (ms)');
+    
+    %% effect congruent - incongruent
+    ylimit = [-0.3, 0.3];
+    figure;
+    hold on;
+    p1 = frevede_errorbarplot(saccade.time, squeeze((d3(:,2,:) - d3(:,3,:)) ./ 2), 'k', 'both'); %should be same as cue-match condition (nr. 5)
+    plot(xlim, [0,0], '--k');
+    plot([0,0], ylimit, '--k');
+    legend([p1], saccade.label(1));
+    xlim(xlimtoplot);
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+
+    %% effect of pressing to cue
+    ylimit = [-0.3, 0.3];
+    figure;
+    hold on;
+    p1 = frevede_errorbarplot(saccade.time, squeeze(d3(:,8,:)), 'b', 'both');
+    p2 = frevede_errorbarplot(saccade.time, squeeze(d3(:,9,:)), 'r', 'both');
+    plot(xlim, [0,0], '--k');
+    plot([0,0], ylimit, '--k');
+    legend([p1, p2], {'pressed', 'did not press'});
+    xlim(xlimtoplot);
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
+
+    %% effect of having to press to cue
+    ylimit = [-0.3, 0.3];
+
+    figure;
+    hold on;
+    p1 = frevede_errorbarplot(saccade.time, squeeze(d3(:,10,:)), 'b', 'both');
+    p2 = frevede_errorbarplot(saccade.time, squeeze(d3(:,11,:)), 'r', 'both');
+    plot(xlim, [0,0], '--k');
+    plot([0,0], ylimit, '--k');
+    legend([p1, p2], {'should have pressed', 'should not have pressed'});
+    xlim(xlimtoplot);
+    ylabel('Rate (Hz)');
+    xlabel('Time (ms)');
 
     %% as function of saccade size
     cfg = [];
@@ -162,23 +220,279 @@ if plotGAs
     % plot([1500,1500], [0, 7], '--', 'LineWidth',3, 'Color', [0.6, 0.6, 0.6]);
     ylim([0.2 6.8]);
 
-end
-%% main figure for paper
-if plotFigures
-    %% towardness overlay of colcue_colprobe and colcue_locprobe
+    %% plot effect of performance (median-split) on saccade data
     figure;
-    hold on;
-    p1 = frevede_errorbarplot(saccade.time, squeeze(d3(:,2,:)), colours(2,:), 'se');
-    p2 = frevede_errorbarplot(saccade.time, squeeze(d3(:,3,:)), colours(2,:), 'se');
-    p3 = frevede_errorbarplot(saccade.time, squeeze(d3(:,4,:)), colours(1,:), 'se');
-    p1.LineWidth = 1.5;
-    p2.LineWidth = 1.5;
-    p3.LineWidth = 1.5;
-    plot(xlim, [0,0], '--k');
-    plot([0,0], ylimit, '--k');
-    legend([p1, p2, p3], saccade.label([2:4]));
+    subplot(2,2,1)
+    hold on
+    c1 = frevede_errorbarplot(saccade.time, squeeze(d3(:,16,:)), [0.5, 0.5, 0.5], 'se');
+    c2 = frevede_errorbarplot(saccade.time, squeeze(d3(:,12,:)), 'b', 'se');
+    legend([c1, c2], {'above median DT', 'below median DT'});
     xlim(xlimtoplot);
-    ylabel('Rate (Hz)');
+    title('Congruent');
+    ylabel('Saccade bias (Hz)');
+
+    subplot(2,2,2)
+    hold on
+    i1 = frevede_errorbarplot(saccade.time, squeeze(d3(:,17,:)), [0.5, 0.5, 0.5], 'se');
+    i2 = frevede_errorbarplot(saccade.time, squeeze(d3(:,13,:)), 'r', 'se');
+    legend([i1, i2], {'above median DT', 'below median DT'});
+    xlim(xlimtoplot);
+    title('Incongruent');
+
+    subplot(2,2,3)
+    hold on
+    c3 = frevede_errorbarplot(saccade.time, squeeze(d3(:,18,:)), [0.5, 0.5, 0.5], 'se');
+    c4 = frevede_errorbarplot(saccade.time, squeeze(d3(:,14,:)), 'b', 'se');
+    legend([c3, c4], {'above median ER', 'below median ER'});
+    xlim(xlimtoplot);
+    ylabel('Saccade bias (Hz)');
+
+    subplot(2,2,4)
+    hold on
+    i3 = frevede_errorbarplot(saccade.time, squeeze(d3(:,19,:)), [0.5, 0.5, 0.5], 'se');
+    i4 = frevede_errorbarplot(saccade.time, squeeze(d3(:,15,:)), 'r', 'se');
+    legend([i3, i4], {'above median ER', 'below median ER'});
+    xlim(xlimtoplot);
+
+    %% plot fast vs. slow (median-split) on saccade data
+    % as timecourse
+    figure;
+    hold on
+    j1 = frevede_errorbarplot(saccade.time, squeeze(d3(:,12,:)) - squeeze(d3(:,16,:)), 'b', 'se');
+    j2 = frevede_errorbarplot(saccade.time, squeeze(d3(:,13,:)) - squeeze(d3(:,17,:)), 'r', 'se');
+    ylabel('Saccade bias (Hz)');
     xlabel('Time (ms)');
-    % ylimit = [-0.3, 0.3];
+    legend([j1, j2], {'Congruent', 'Incongruent'});
+    xlim(xlimtoplot);
+    title('Fast vs. slow effect');
+
+    % as bargraph
+    figure;
+    hold on
+    b1 = bar([1,2], mean(avg_saccade_effect(:,1:2)), 0.6);
+    b2 = bar([3,4], mean(avg_saccade_effect(:,3:4)), 0.6);
+    errorbar(mean(avg_saccade_effect), std(avg_saccade_effect) ./ sqrt(size(pp2do,2)), 'LineStyle', 'None', 'Color', 'k');
+    ylabel('Mean saccade rate (ΔHz)');
+    xticks([1,2,3,4]);
+    xticklabels({'fast', 'slow', 'fast', 'slow'});
+    legend({'congruent', 'incongruent'});
+    
+    % first level = condition, second level = fast or slow
+    stats = rm_anova2([avg_saccade_effect(:,1);avg_saccade_effect(:,2);avg_saccade_effect(:,3);avg_saccade_effect(:,4)], repmat([1:25]',4,1), [ones(50,1);ones(50,1)*2], [ones(25,1);ones(25,1)*2;ones(25,1);ones(25,1)*2], {'condition',  'speed'})
+    
+    [h,p,ci,stats] = ttest(avg_saccade_effect(:,1), avg_saccade_effect(:,2))
+    [h,p,ci,stats] = ttest(avg_saccade_effect(:,3), avg_saccade_effect(:,4))
+end
+%% main figures for poster
+if plotFigures
+    %% saccade bias post-cue, including size course
+    %some settings
+    ylim1 = [0, 1];
+    ylim2 = [-0.2, 0.4];
+    line = 2;
+    x_ticks = [0, 500, 1000, 1500];
+
+    cfg = [];
+    cfg.parameter = 'effect';
+    cfg.figure = 'gcf';
+    cfg.zlim = [-0.12, 0.12];
+    cfg.xlim = xlimtoplot;
+    cfg.colormap = brewermap(1000, 'RdBu');
+
+    statcfg.xax = saccade.time;
+    statcfg.npermutations = 10000;
+    statcfg.clusterStatEvalaluationAlpha = 0.05;
+    statcfg.nsub = size(pp2do, 2);
+    statcfg.statMethod = 'montecarlo';
+    
+    timeframe = [951:2451]; %0 - 1500 ms post-cue
+    stat = frevede_ftclusterstat1D(statcfg, squeeze(d3(:,5,timeframe)), zeros(size(squeeze(d3(:,5,timeframe)))));
+    mask = double(stat.mask); mask(mask==0) = nan; % nan data that is not part of mark
+    
+   %%
+    % make figure 🎉
+    figure;
+
+    % cue-matching
+    tL = subplot(1,3,1);
+    hold on
+    p1 = frevede_errorbarplot(saccade.time, squeeze(d1(:,5,:)), get_colour("blue", ""), 'se');
+    p2 = frevede_errorbarplot(saccade.time, squeeze(d2(:,5,:)), get_colour("red", ""), 'se');
+    p1.LineWidth = line;
+    p2.LineWidth = line;
+    legend([p1, p2], {'toward', 'away'}, 'EdgeColor','none', 'AutoUpdate','off', 'FontSize', 17);
+    xline(0, 'LineWidth',2, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    xlim(xlimtoplot);
+    xticks(x_ticks);
+    ylim(ylim1);
+
+    yticks([0, 0.5, 1]);
+    ylabel('Saccade rate (Hz)');
+
+    mL = subplot(1,3,2);
+    hold on
+    p3 = frevede_errorbarplot(saccade.time, squeeze(d3(:,5,:)), get_colour("pink", ""), 'se');
+    p3.LineWidth = line;
+    yline(0, 'LineWidth',2, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    xline(0, 'LineWidth',2, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    sig = plot(saccade.time(timeframe), mask*-0.15, 'Color', get_colour("pink",""), 'LineWidth', 3);
+    xlim(xlimtoplot);
+    xticks(x_ticks);
+    ylim(ylim2);
+    yticks([-0.2 0 0.2 0.4]);
+    ylabel('Saccade bias (ΔHz)');
+    
+    bL = subplot(1,3,3);
+    cfg.channel = 5;
+    ft_singleplotTFR(cfg, saccadesize);
+    yline(6-4/2, 'LineWidth',2, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    xline(0, 'LineWidth',2, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    c= colorbar();
+    c.Position = [0.92 0.32 0.012 0.41];
+    c.Ticks = [-0.1, 0, 0.1];
+    xticks(x_ticks);
+    ylim([0.25, 6]);
+    ylabel('Saccade size (degrees)');
+    title([]);
+    
+    % general
+    set(gcf(), 'Position', [500 500 1450 370])
+
+    axes = {tL, mL, bL};
+    for i = 1:size(axes,2)
+        xlabel(axes{i}, 'Time after cue (ms)', 'FontName', 'Aptos');
+        set(axes{i}, 'Box', 'on');
+        set(axes{i}, 'FontSize', [17]);
+        set(axes{i}, 'FontName', 'Aptos');
+        set(axes{i}, 'LineWidth', 1);
+        
+    end
+
+    print("C:\Users\annav\Documents\Surfdrive\Conferences\ICON 2025\Figures\saccade_post_cue", "-dsvg")
+    print("C:\Users\annav\Documents\Surfdrive\Conferences\ICON 2025\Figures\saccade_post_cue", "-dpng")
+
+    %% effect on attentional selection latency (bias post-probe)
+    line = 2;
+
+    figure;
+    top = subplot(5,1,[1:4]);
+    hold on
+    p1 = frevede_errorbarplot(saccade.time, squeeze(d3(:,2,:)), get_colour("blue", ""), 'se');
+    p2 = frevede_errorbarplot(saccade.time, squeeze(d3(:,3,:)), get_colour("green", ""), 'se');
+    p3 = frevede_errorbarplot(saccade.time, squeeze(d3(:,4,:)), get_colour("red", ""), 'se');
+    p1.LineWidth = 3;
+    p2.LineWidth = 3;
+    p3.LineWidth = 3;
+    xline(1500, 'LineWidth', 3, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    yline(0, 'LineWidth',3, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    ylim([-0.2 0.5]);
+    yticks([-0.2, 0, 0.2, 0.4]);
+    xlim([1400, 2400]);
+    xticks([1500, 1800, 2100, 2400]);
+    xticklabels({});
+    ylabel('Saccade bias (ΔHz)');
+    legend([p1, p2, p3], {'match tested', 'match none', 'match untested'}, 'EdgeColor', 'none', 'FontSize', 19.5);
+
+    %get jackknife estimation of latency + stats
+    % time = 2650:3050;  % 200 - 600 ms post probe cue onset
+    time = 2450:3250;  % 0 - 800 ms post probe cue onset
+    nTime = length(time);
+    
+    % Function to get latency of peak (or half-peak)
+    get_halfpeak_latency = @(tc, t) ...
+        t(find(mean(tc, 2) >= 0.5 * max(mean(tc, 2)), 1, 'first'));  % Half-peak (onset)
+    
+    % Initialize matrices to store jackknife latency values
+    peakLatencies = zeros(size(pp2do,2), 3);
+    halfLatencies = zeros(size(pp2do,2), 3);
+    halfDownLatencies = zeros(size(pp2do,2), 3);
+
+    % Single pp loop: calculate peak latencies and half-peak latencies for each participant
+    for i = 1:size(pp2do,2)
+        
+        [~, pp_peakLatencies(i, 1)] = max(squeeze(d3(i,2,time)));
+        [~, pp_peakLatencies(i, 2)] = max(squeeze(d3(i,3,time)));
+        [~, pp_peakLatencies(i, 3)] = max(squeeze(d3(i,4,time)));
+        
+                                   % half peak cannot be before full peak!
+        [~, pp_halfLatencies(i, 1)] = min(abs(squeeze(d3(i,2,time(1:pp_peakLatencies(i,1)))) - max(squeeze(d3(i,2,time))) / 2));
+        [~, pp_halfLatencies(i, 2)] = min(abs(squeeze(d3(i,3,time(1:pp_peakLatencies(i,2)))) - max(squeeze(d3(i,3,time))) / 2));
+        [~, pp_halfLatencies(i, 3)] = min(abs(squeeze(d3(i,4,time(1:pp_peakLatencies(i,3)))) - max(squeeze(d3(i,4,time))) / 2));
+
+        [~, pp_halfDownLatencies(i, 1)] = min(abs(squeeze(d3(i,2,time(pp_peakLatencies(i,1):end))) - max(squeeze(d3(i,2,time))) / 2));
+        [~, pp_halfDownLatencies(i, 2)] = min(abs(squeeze(d3(i,3,time(pp_peakLatencies(i,2):end))) - max(squeeze(d3(i,3,time))) / 2));
+        [~, pp_halfDownLatencies(i, 3)] = min(abs(squeeze(d3(i,4,time(pp_peakLatencies(i,3):end))) - max(squeeze(d3(i,4,time))) / 2));
+    end
+
+    pp_halfDownLatencies = pp_halfDownLatencies + pp_peakLatencies;
+
+    
+    % Jackknife loop: leave-one-participant-out
+    for i = 1:size(pp2do,2)
+        idx = setdiff(1:size(pp2do,2), i);  % all participants except i
+        
+        % Compute latency estimates with one pp removed
+        [~, peakLatencies(i, 1)] = max(mean(squeeze(d3(idx,2,time))));
+        [~, peakLatencies(i, 2)] = max(mean(squeeze(d3(idx,3,time))));
+        [~, peakLatencies(i, 3)] = max(mean(squeeze(d3(idx,4,time))));
+
+                                   % half peak cannot be before full peak!
+        [~, halfLatencies(i, 1)] = min(abs(mean(squeeze(d3(idx,2,time(1:peakLatencies(i,1))))) - max(mean(squeeze(d3(idx,2,time)))) / 2));
+        [~, halfLatencies(i, 2)] = min(abs(mean(squeeze(d3(idx,3,time(1:peakLatencies(i,2))))) - max(mean(squeeze(d3(idx,3,time)))) / 2));
+        [~, halfLatencies(i, 3)] = min(abs(mean(squeeze(d3(idx,4,time(1:peakLatencies(i,3))))) - max(mean(squeeze(d3(idx,4,time)))) / 2));
+
+        [~, halfDownLatencies(i, 1)] = min(abs(mean(squeeze(d3(idx,2,time(peakLatencies(i,1):end)))) - max(mean(squeeze(d3(idx,2,time)))) / 2));
+        [~, halfDownLatencies(i, 2)] = min(abs(mean(squeeze(d3(idx,3,time(peakLatencies(i,2):end)))) - max(mean(squeeze(d3(idx,3,time)))) / 2));
+        [~, halfDownLatencies(i, 3)] = min(abs(mean(squeeze(d3(idx,4,time(peakLatencies(i,3):end)))) - max(mean(squeeze(d3(idx,4,time)))) / 2));
+    end
+
+    halfDownLatencies = halfDownLatencies + peakLatencies;
+
+    % compute stats using jackknife adjusted formula
+    % start by estimating individual latencies
+    for i = 1:size(pp2do,2)
+        halfLatency_ests(i,1) = size(pp2do, 2)*mean(halfLatencies(:,1)) - (size(pp2do, 2) - 1)*halfLatencies(i,1);
+        halfLatency_ests(i,2) = size(pp2do, 2)*mean(halfLatencies(:,2)) - (size(pp2do, 2) - 1)*halfLatencies(i,2);
+        halfLatency_ests(i,3) = size(pp2do, 2)*mean(halfLatencies(:,3)) - (size(pp2do, 2) - 1)*halfLatencies(i,3);
+    
+        peakLatency_ests(i,1) = size(pp2do, 2)*mean(peakLatencies(:,1)) - (size(pp2do, 2) - 1)*peakLatencies(i,1);
+        peakLatency_ests(i,2) = size(pp2do, 2)*mean(peakLatencies(:,2)) - (size(pp2do, 2) - 1)*peakLatencies(i,2);
+        peakLatency_ests(i,3) = size(pp2do, 2)*mean(peakLatencies(:,3)) - (size(pp2do, 2) - 1)*peakLatencies(i,3);
+
+        halfDownLatency_ests(i,1) = size(pp2do, 2)*mean(halfDownLatencies(:,1)) - (size(pp2do, 2) - 1)*halfDownLatencies(i,1);
+        halfDownLatency_ests(i,2) = size(pp2do, 2)*mean(halfDownLatencies(:,2)) - (size(pp2do, 2) - 1)*halfDownLatencies(i,2);
+        halfDownLatency_ests(i,3) = size(pp2do, 2)*mean(halfDownLatencies(:,3)) - (size(pp2do, 2) - 1)*halfDownLatencies(i,3);
+    end
+
+    bottom = subplot(5,1,[5]);
+    hold on
+    xline(1500, 'LineWidth',3, 'Color',[107, 107, 107]/255, 'LineStyle','--');
+    scatter(mean(halfDownLatencies)+saccade.time(time(1)), [1,2,3], 100, [get_colour("blue", "");get_colour("green", "");get_colour("red", "")], 'filled');
+    errorbar(mean(halfDownLatencies(:,1))+saccade.time(time(1)),[1], std(halfDownLatency_ests(:,1)) ./ sqrt(size(pp2do,2)), "horizontal", 'LineStyle', 'none', 'Color', get_colour("blue", ""), 'LineWidth', 3);
+    errorbar(mean(halfDownLatencies(:,2))+saccade.time(time(1)),[2], std(halfDownLatency_ests(:,2)) ./ sqrt(size(pp2do,2)), "horizontal", 'LineStyle', 'none', 'Color', get_colour("green", ""), 'LineWidth', 3);
+    errorbar(mean(halfDownLatencies(:,3))+saccade.time(time(1)),[3], std(halfDownLatency_ests(:,3)) ./ sqrt(size(pp2do,2)), "horizontal", 'LineStyle', 'none', 'Color', get_colour("red", ""), 'LineWidth', 3);
+    ylim([0,4]);
+    yticks([]);
+    xlim([1400, 2400]);
+    xticks([1500, 1800, 2100, 2400]);
+    xticklabels({'0', '300', '600', '900'});
+    xlabel('Time after probe (ms)');
+    % set(gca(), 'Position', [0.1300 0.0450 0.7750 0.1243]);
+
+
+    % general
+    set(gcf(), 'Position', [500 500 850 800])
+
+    axes = {top, bottom};
+    for i = 1:size(axes,2)
+        set(axes{i}, 'Box', 'on');
+        set(axes{i}, 'FontSize', [25.4]);
+        set(axes{i}, 'FontName', 'Aptos');
+        set(axes{i}, 'LineWidth', 1.49);
+    end
+
+    print("C:\Users\annav\Documents\Surfdrive\Conferences\ICON 2025\Figures\saccade_post_probe", "-dsvg")
+    print("C:\Users\annav\Documents\Surfdrive\Conferences\ICON 2025\Figures\saccade_post_probe", "-dpng")
+
+    
 end

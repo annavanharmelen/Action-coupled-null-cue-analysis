6a2d3b798c232676361110c56fdc3ee8092389f6
Anna <anna@vanharmelen.me>
2025-10-15 10:55:24 +0200

    update data dir

--- getSubjParam.m
+++ getSubjParam.m
@@ -5,7 +5,7 @@ function param = getSubjParam(pp)
 %% set path and pp-specific file locations
 unique_numbers = [25,44,43,24,59,13,14,63,37,21,62,64,42,71,35,89,61,97,26,78,91,47,70,94,88,46]; %needs to be in the right order
 
-param.path = '\\labsdfs.labs.vu.nl\labsdfs\FGB-ETP-CogPsy-ProactiveBrainLab\core_lab_members\Anna\Data\vidi5 - action-coupled-null-cue\';
+param.path = '\\scistor.vu.nl\shares\FGB-ETP-CogPsy-ProactiveBrainLab\core_lab_members\Anna\Data\vidi5 - action-coupled-null-cue\';
 
 if pp < 10
     param.subjName = sprintf('pp0%d', pp);
